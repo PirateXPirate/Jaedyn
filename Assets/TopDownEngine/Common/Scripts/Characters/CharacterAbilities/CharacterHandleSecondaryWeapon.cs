03a5ccd7bdcb53a0ff95d1bff94f9a8a458c5835
aladinhowatt <38517212+aladinhowatt@users.noreply.github.com>
2023-02-17 01:06:36 +0700

    update anatar scale , Add jump animation

--- Assets/TopDownEngine/Common/Scripts/Characters/CharacterAbilities/CharacterHandleSecondaryWeapon.cs
+++ Assets/TopDownEngine/Common/Scripts/Characters/CharacterAbilities/CharacterHandleSecondaryWeapon.cs
@@ -72,5 +72,38 @@ namespace MoreMountains.TopDownEngine
 				ShootStart();
 			}
 		}
-	}
+        public override void ShootStart()
+        {
+			// if the Shoot action is enabled in the permissions, we continue, if not we do nothing.  If the player is dead we do nothing.
+			if (!AbilityAuthorized
+				|| (CurrentWeapon == null)
+				|| (_condition.CurrentState != CharacterStates.CharacterConditions.Normal))
+			{
+				return;
+			}
+			if (skillUiManager)
+				skillUiManager.SetSkillCooldown(coolDown);
+			//  if we've decided to buffer input, and if the weapon is in use right now
+			if (BufferInput && (CurrentWeapon.WeaponState.CurrentState != Weapon.WeaponStates.WeaponIdle))
+			{
+				// if we're not already buffering, or if each new input extends the buffer, we turn our buffering state to true
+				ExtendBuffer();
+			}
+
+			if (BufferInput && RequiresPerfectTile && (_characterGridMovement != null))
+			{
+				if (!_characterGridMovement.PerfectTile)
+				{
+					ExtendBuffer();
+					return;
+				}
+				else
+				{
+					_buffering = false;
+				}
+			}
+			PlayAbilityStartFeedbacks();
+			CurrentWeapon.WeaponInputStart();
+		}
+    }
 }
