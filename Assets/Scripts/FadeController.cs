21521fe7d6920f9422737546a335f2bd416c5d12
brajaobell <polpanu@gmail.com>
2023-02-17 19:35:07 +0700

    Startgame Scene

---
+++ Assets/Scripts/FadeController.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeController : MonoBehaviour
+{
+    const float FLASH_SCENE_TIME = 1.0f;
+
+    [Header("white screen")]
+    [SerializeField] CanvasGroup fadeCanvas;
+    [SerializeField] float fadeDuration = 1f;
+
+    [Header("button opacity")]
+    [SerializeField] CanvasGroup fadeButton;
+    [SerializeField] float buttonFadeDuration;
+
+    public GameObject dialogPanel;
+    public CanvasGroup dialogGroupForFade;
+    public bool isPlayButtonPressed;
+    public bool isGotoMapScenePressed;
+
+
+
+    void Start()
+    {
+        StartCoroutine(FadeIntro());
+    }
+
+    void Update()
+    {
+        if (isPlayButtonPressed)
+        {
+            StartCoroutine(FadeToDialog());
+        }
+
+        if (isGotoMapScenePressed)
+        {
+            isGotoMapScenePressed = false;
+            fadeCanvas.gameObject.SetActive(true);
+            StartCoroutine(FadeOut(fadeCanvas,fadeDuration));
+        }
+    }
+
+    IEnumerator FadeIntro()
+    {
+        StartCoroutine(FadeIn(fadeCanvas, fadeDuration));
+        yield return new WaitForSeconds(fadeDuration + FLASH_SCENE_TIME);
+        StartCoroutine(FadeOut(fadeButton, buttonFadeDuration));
+        yield return new WaitForSeconds(buttonFadeDuration);
+        fadeCanvas.gameObject.SetActive(false);
+        yield return null;
+    }
+
+    public IEnumerator FadeToDialog()
+    {
+        isPlayButtonPressed = false;
+        fadeCanvas.gameObject.SetActive(true);
+        StartCoroutine(FadeOut(fadeCanvas, fadeDuration));
+        yield return new WaitForSeconds(fadeDuration);
+        dialogPanel.SetActive(true);
+        StartCoroutine(FadeIn(fadeCanvas, fadeDuration));
+        yield return new WaitForSeconds(fadeDuration);
+        StartCoroutine(FadeOut(dialogGroupForFade, fadeDuration));
+        yield return new WaitForSeconds(fadeDuration);
+        fadeCanvas.gameObject.SetActive(false);
+        yield return null;
+    }
+
+    public IEnumerator FadeOut(CanvasGroup valve, float duration)
+    {
+        float timeElapsed = 0;
+        while (timeElapsed < duration)
+        {
+            valve.alpha = Mathf.Lerp(0, 1, timeElapsed / duration);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+        valve.alpha = 1;
+    }
+
+    public IEnumerator FadeIn(CanvasGroup valve, float duration)
+    {
+        float timeElapsed = 0;
+        while (timeElapsed < duration)
+        {
+            valve.alpha = Mathf.Lerp(1, 0, timeElapsed / duration);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+        valve.alpha = 0;
+    }
+
+}
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    FadeController FadeController;
+
+    [Header("Button")]
+    [SerializeField] Button galleryButton;
+    [SerializeField] Button playButton;
+    [SerializeField] Button settingButton;
+    [SerializeField] Button goMapSceneButton;
+
+    [SerializeField] GameObject buttonMenu;
+    [SerializeField] GameObject headerMenu;
+    private void Awake()
+    {
+        FadeController = gameObject.GetComponent<FadeController>();
+        SetListenner();
+    }
+
+    void SetListenner()
+    {
+        galleryButton.onClick.AddListener(GalleryButton);
+        playButton.onClick.AddListener(PlayButton);
+        settingButton.onClick.AddListener(SettingButton);   
+    }
+
+    void GalleryButton()
+    {
+        Debug.Log("GalleryButton Press!!");
+        //SceneManager.LoadSceneAsync("", LoadSceneMode.Single);
+    }
+    void PlayButton()
+    {
+        FadeController.isPlayButtonPressed = true;
+        StartCoroutine(DeleteMenuUi());
+        StartCoroutine(ReadyToGoMapScene());
+    }
+
+    void SettingButton()
+    {
+        Debug.Log("SettingButton Press!!");
+        //SceneManager.LoadSceneAsync("", LoadSceneMode.Single);
+    }
+
+    void GoMapSceneButton()
+    {
+        StartCoroutine(GoMapScene());
+    }
+
+    IEnumerator DeleteMenuUi()
+    {
+        yield return new WaitForSeconds(2.0f);
+        buttonMenu.SetActive(false);
+        headerMenu.SetActive(false);
+        yield return null;
+    }
+
+    IEnumerator ReadyToGoMapScene()
+    {
+        float delayafterpressplay = 7.0f;
+
+        yield return new WaitForSeconds(delayafterpressplay);
+        goMapSceneButton.gameObject.SetActive(true);
+        goMapSceneButton.onClick.AddListener(GoMapSceneButton);
+        yield return null;
+    }
+
+    IEnumerator GoMapScene()
+    {
+        float waitforfade = 2.0f;
+
+        FadeController.isGotoMapScenePressed = true;
+        yield return new WaitForSeconds(waitforfade);
+        SceneManager.LoadSceneAsync("MapScene", LoadSceneMode.Single);
+        yield return null;
+    }
+}
+    
