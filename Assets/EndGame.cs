a85f591bd305ced18979bc529825ac91c4cc4e24
brajaobell <polpanu@gmail.com>
2023-02-22 16:25:19 +0700

    Endgame Level Checker

--- Assets/EndGame.cs
+++ Assets/EndGame.cs
@@ -6,18 +6,67 @@ using UnityEngine.SceneManagement;
 public class EndGame : MonoBehaviour
 {
     [SerializeField] private GameObject controllerUi;
-    void Start()
+    [SerializeField] private GameObject picture;
+
+    [SerializeField] private bool isTutorialLevel = false;
+    [SerializeField] private int levelIndex = 0;
+    private bool isPictureTaken = false;
+    [SerializeField] private int levelState;
+
+    [SerializeField] private enum Mode { easy, hard }
+    [SerializeField] private Mode mode;
+
+    private void Awake()
     {
         controllerUi.SetActive(false);
+        CheckPictureInScene();
+        CheckEndGameState();
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        SaveToPlayerPerf();
+    }
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             gameObject.SetActive(false);
             SceneManager.LoadScene("MapScene");
+        }
+    }
+
+    void CheckPictureInScene()
+    {
+        if (!picture.activeSelf)
+        {
+            isPictureTaken = true;
+        }
+    }
+
+    void CheckEndGameState()
+    {
+        if (isPictureTaken)
+        {
+            levelState = 2;
+        }
+
+        else
+        {
+            levelState = 1;
+        }
+    }
+    void SaveToPlayerPerf()
+    {
+        if (isTutorialLevel)
+        {
+            LevelData.TutorialComplete();
+            return;
+        }
+
+        else
+        {
+            LevelData.SaveLevelStateData(mode.ToString(), levelIndex, levelState);
         }
     }
 }
